c9a5db264d263cc86b727cf49367d1ff7d8650d1
Marcelo Oliveira <mfoliveira88@gmail.com>
2021-02-26 16:12:29 -0300

    reservas no profile

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,12 @@ class Reservation < ApplicationRecord
   belongs_to :trailer
 
   validates :start_date, :end_date, presence: true
+
+  def info
+    "#{trailer.model}\n
+    De #{start_date.strftime("%Y/%m/%d")} a #{end_date.strftime("%Y/%m/%d")}\n
+    Preço da diária: #{price}\n
+    Preço Total: #{total}\n
+    Endereço: #{trailer.address}"
+  end
 end
