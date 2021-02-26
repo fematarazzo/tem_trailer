968eec695a5160ae6ffd7c4f2a200868043197d8
Felipe Matarazzo <felipemps@protonmail.com>
2021-02-26 12:56:08 -0300

    ajustes das datas e outros ajustes finos

--- app/controllers/trailers_controller.rb
+++ app/controllers/trailers_controller.rb
@@ -2,16 +2,19 @@ class TrailersController < ApplicationController
   before_action :set_trailer, only: %i[show edit update destroy]
   # before_action :validate_current_user, only: %i[edit update destroy]
   def index
-    if params[:search].present?
+    unless params[:search][:start_date].blank? && params[:search][:end_date].blank? && params[:search][:query].blank? && params[:search][:capacity].blank?
       # start_date = Date.new(params[:"reservation"][:"start_date(1i)"].to_i, params[:"reservation"][:"start_date(2i)"].to_i, params[:"reservation"][:"start_date(3i)"].to_i)
       # end_date = Date.new(params[:"reservation"][:"end_date(1i)"].to_i, params[:"reservation"][:"end_date(2i)"].to_i, params[:"reservation"][:"end_date(3i)"].to_i)
       # @trailers = Trailer.search_by_address(params[:query]
       @reservation = Reservation.new(start_date: params[:search][:start_date], end_date: params[:search][:end_date])
-
-      @trailers = Trailer.where("address ILIKE ?", "%#{params[:search][:query]}%")
-                         .where(onboard_capacity: params[:search][:capacity])
-      @trailers.select do |trailer|
-        Reservation.where(trailer: trailer).where("start_date > ? OR end_date <  ?", params[:search][:end_date], params[:search][:start_date]).empty?
+      if params[:search][:capacity].blank?
+        @trailers = Trailer.where("address ILIKE ?", "%#{params[:search][:query]}%")
+      else
+        @trailers = Trailer.where("address ILIKE ?", "%#{params[:search][:query]}%")
+                           .where(onboard_capacity: params[:search][:capacity])
+        @trailers.select do |trailer|
+          Reservation.where(trailer: trailer).where("start_date > ? OR end_date <  ?", params[:search][:end_date], params[:search][:start_date]).empty?
+        end
       end
       @markers = @trailers.geocoded.map do |flat|
         {
